cd86159969b8a54028b9e1fad6cf34ea12d5fec2
Branchie <michaelsemps@hotmail.co.uk>
2019-09-20 21:04:12 +0200

    Pause management.

--- SpicierPorky/Assets/Scripts/Managers/GameManager.cs
+++ SpicierPorky/Assets/Scripts/Managers/GameManager.cs
@@ -4,21 +4,13 @@
 
 	public class GameManager : MonoBehaviour
 	{
-		private bool paused = false;
-
-		private void Awake()
-		{
-			if (GetComponentInChildren<InputManager>() == null)
-				gameObject.AddComponent<InputManager>();
-		}
-
 		private void Update()
 		{
 			if (Input.GetKeyDown(KeyCode.R))
 				ResetManager.ResetAll();
 
 			if (Input.GetKeyDown(KeyCode.P))
-				SuspensionManager.SetSuspendedAll(paused = !paused);
+				SuspensionManager.SetSuspendedAll(PauseManager.Toggle());
 		}
 	}
 }
