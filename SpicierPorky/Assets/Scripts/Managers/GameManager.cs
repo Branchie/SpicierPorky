e402d0fb03aec27d452a1688c1c1b9d359086291
Branchie <michaelsemps@hotmail.co.uk>
2019-11-23 02:37:25 +0100

    Better pause / reset temporary support.

--- SpicierPorky/Assets/Scripts/Managers/GameManager.cs
+++ SpicierPorky/Assets/Scripts/Managers/GameManager.cs
@@ -1,15 +1,22 @@
 ﻿namespace Gypo.SpicierPorky
 {
+	using Gypo.Input;
 	using UnityEngine;
 
 	public class GameManager : MonoBehaviour
 	{
+		private InputProcessor pause = new InputProcessor();
+		private InputProcessor reset = new InputProcessor();
+
 		private void Update()
 		{
-			if (Input.GetKeyDown(KeyCode.R))
+			pause.Update(Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Escape) || Gamepad.GetButton(Gamepad.Button.Start));
+			reset.Update(Input.GetKeyDown(KeyCode.R) || Gamepad.GetButton(Gamepad.Button.Y));
+
+			if (reset.onPressed)
 				ResetManager.ResetAll();
 
-			if (Input.GetKeyDown(KeyCode.P))
+			if (pause.onPressed)
 				SuspensionManager.SetSuspendedAll(PauseManager.Toggle());
 		}
 	}
